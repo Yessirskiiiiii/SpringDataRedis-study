3015366143e292967c2e239aaaeee43eb0d7e7a7
BaoShaWang <674134561@qq.com>
2022-03-08 18:36:26 +0800

    Initial Commit

---
+++ src/main/java/com/threewater/Springboot05RedisApplication.java
@@ -0,0 +1,13 @@
+package com.threewater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot05RedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot05RedisApplication.class, args);
+    }
+
+}
