0f6a5b758b4c661d23d7af5ece80ff7d367dc470
BaoShaWang <674134561@qq.com>
2022-03-08 18:57:49 +0800

    Redis快速使用

--- src/test/java/com/threewater/Springboot05RedisApplicationTests.java
+++ src/test/java/com/threewater/Springboot05RedisApplicationTests.java
@@ -1,13 +1,23 @@
 package com.threewater;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
 class Springboot05RedisApplicationTests {
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @Test
     void contextLoads() {
+        // 写入一条String数据
+        redisTemplate.opsForValue().set("name", "zjl");
+        // 获取String数据
+        Object name = redisTemplate.opsForValue().get("name");
+        System.out.println(name);
     }
 
 }
